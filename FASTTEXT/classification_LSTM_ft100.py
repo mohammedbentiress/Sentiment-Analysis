241c1eb492ae940739e0ef5543f717240279787d
Fatima Bellafkih <50462810+fatibellafkih@users.noreply.github.com>
2020-06-07 02:54:53 +0100

    Update fasttext classifier

--- FASTTEXT/classification_LSTM_ft100.py
+++ FASTTEXT/classification_LSTM_ft100.py
@@ -16,8 +16,8 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.layers import Activation, Dropout, Dense, Flatten, GlobalMaxPool1D, Embedding, Conv1D, LSTM, GRU,Bidirectional,SpatialDropout1D
 from gensim.models import fasttext
 
-dataset_file = 'Classification/DATA/clean_tweets.csv'
-ft_file = 'Classification/FASTTEXT/cc.en.100.bin'
+dataset_file = 'DATA/clean_tweets.csv'
+ft_file = 'FASTTEXT/cc.en.100.bin'
 maxlen = 100
 
 """ define functions for treatement """ 
@@ -163,7 +163,7 @@ print('\n prediction for \n',prediction[:,0])
 # save model and weights
 save_model(
     model,
-    "Classification/FASTTEXT/models/model_LSTM_ft100.h5",
+    "FASTTEXT/models/model_LSTM_ft100.h5",
     overwrite=True,
     include_optimizer=True
 )
